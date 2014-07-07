e6673cbef93bdd4a166a15241df499cc6762004c
zhaijm <zhaijm@zhaijm-PC>
2014-07-07 23:26:18 +0800

    添加一个基本类，Code

---
+++ src/info/zhaijm2014/tools/codestore/things/Code.java
@@ -0,0 +1,56 @@
+/**
+ * Project Name:CodeStore
+ * File Name:Code.java
+ * Package Name:info.zhaijm2014.tools.codestore.things
+ * Date:2014年7月7日下午11:24:04
+ * Copyright (c) 2014, chenzhou1025@126.com All Rights Reserved.
+ *
+ */
+
+package info.zhaijm2014.tools.codestore.things;
+
+/**
+ * ClassName:Code <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2014年7月7日 下午11:24:04 <br/>
+ * 
+ * @author zhaijm
+ * @version
+ * @since JDK 1.6
+ * @see
+ */
+public class Code {
+	private String text;
+	private String textType;
+	private String textStyle;
+
+	public void format() {
+
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getTextType() {
+		return textType;
+	}
+
+	public void setTextType(String textType) {
+		this.textType = textType;
+	}
+
+	public String getTextStyle() {
+		return textStyle;
+	}
+
+	public void setTextStyle(String textStyle) {
+		this.textStyle = textStyle;
+	}
+
+}
